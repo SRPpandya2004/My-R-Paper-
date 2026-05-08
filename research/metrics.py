9c666e74da07539e70970bc87cbac7cdd560d9a7
Shaunak Pandya <176204036+SRPpandya2004@users.noreply.github.com>
2026-05-08 10:58:32 +0530

    Delete research/metrics.py

--- research/metrics.py
+++
@@ -1,8 +0,0 @@
-def top_k_accuracy(results, ground_truth, k=1):
-    correct = 0
-
-    for res, gt in zip(results, ground_truth):
-        if gt in res[:k]:
-            correct += 1
-
-    return correct / len(results)
